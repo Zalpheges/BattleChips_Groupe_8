2ab667f36f42b9dbb78b07d64952586dbcdcf44b
Zalpheges <geoffreey.bernard@gmail.com>
2022-03-06 11:30:28 +0100

    Server OK

---
+++ Assets/ClientManager.cs
@@ -0,0 +1,237 @@
+using System.Collections.Generic;
+using PlayerIOClient;
+using UnityEngine;
+using TMPro;
+
+public class ClientManager : MonoBehaviour
+{
+	private Queue<Message> messages;
+
+	private Connection server;
+
+	private static ClientManager instance;
+
+    private void Awake()
+    {
+		if (instance == null)
+        {
+            instance = this;
+			DontDestroyOnLoad(gameObject);
+        }
+        else
+			Destroy(gameObject);
+    }
+
+    private void Start()
+    {
+		messages = new Queue<Message>();
+
+		if (PlayerPrefs.HasKey("Username"))
+        {
+			username.text = PlayerPrefs.GetString("Username");
+			roomname.text = PlayerPrefs.GetString("Roomname");
+		}
+	}
+
+	public void Connect()
+    {
+		if (username.text.Length < 3 || roomname.text.Length < 3)
+			return;
+
+		PlayerIO.Authenticate(
+			"battlechips-tmwm0lz8memju96zesetw",
+			"public",
+			new Dictionary<string, string> {
+				{ "userId", username.text },
+			},
+			null,
+			delegate (Client client) {
+				Debug.Log("Successfully connected to Player.IO");
+
+				client.Multiplayer.DevelopmentServer = new ServerEndpoint("localhost", 8184);
+
+				client.Multiplayer.CreateJoinRoom(
+					roomname.text,
+					"BattleChip",
+					true,
+					null,
+					null,
+					delegate (Connection connection) {
+						Debug.Log("Joined Room.");
+
+						PlayerPrefs.SetString("Username", username.text);
+						PlayerPrefs.SetString("Roomname", roomname.text);
+
+						server = connection;
+						server.OnMessage += OnMessage;
+						server.OnDisconnect += OnDisconnect;
+
+						server.Send("Count");
+						ShowMenu(Menu.Ready);
+					},
+					delegate (PlayerIOError error) {
+						Debug.Log("Error Joining Room: " + error.ToString());
+
+						ShowMenu(Menu.Connect);
+					}
+				);
+			},
+			delegate (PlayerIOError error) {
+				Debug.Log("Error connecting: " + error.ToString());
+
+				ShowMenu(Menu.Connect);
+			}
+		);
+	}
+
+	public void Ready(bool state)
+    {
+		server.Send("Ready", state);
+    }
+
+	public static void AddShip(int x, int y, int dir, int length)
+    {
+		instance.server.Send("Add", x, y, dir, length);
+	}
+
+	public static void RemoveShip(int x, int y, int dir, int length)
+	{
+		instance.server.Send("Add", x, y, dir, length);
+	}
+
+	private void FixedUpdate()
+	{
+		while (messages.Count > 0)
+		{
+			Message message = messages.Dequeue();
+
+			switch (message.Type)
+			{
+				case "Board":
+                {
+					int id = message.GetInt(0);
+
+					ShowMenu(Menu.None);
+
+					// Lancer la selection de board et répondre "Boarded" quand terminé
+
+					break;
+                }
+
+				case "Count":
+                {
+					int ready = message.GetInt(0);
+					int total = message.GetInt(1);
+
+					playerCount.text = ready + "/" + total + " prêt" + (ready > 1 ? "s" : "");
+
+					break;
+                }
+
+				case "Shoot":
+                {
+					int id = message.GetInt(0);
+					int x = message.GetInt(1);
+					int y = message.GetInt(2);
+
+					bool touched = message.GetBoolean(3);
+					bool destroyed = message.GetBoolean(4);
+
+					if (touched) {
+						// Lancer le missile avec la variable destroyed pour indiquer s'il faut afficher le bateau coulé pendant l'animation
+                    }
+					else {
+						// Case de vient rouge
+                    }
+
+					break;
+                }
+
+				case "Play":
+                {
+					// La partie vient de commencer tous les joueurs ont répondu "Boarded"
+
+					break;
+                }
+
+				case "Dead":
+                {
+					int id = message.GetInt(0);
+
+					// Le joueur id vient de perdre son dernier vaisseau
+
+					break;
+                }
+
+				case "End":
+                {
+					int winner = message.GetInt(0);
+
+					ShowMenu(Menu.End);
+
+					break;
+                }
+			}
+		}
+	}
+
+	private void OnMessage(object sender, Message message)
+	{
+		messages.Enqueue(message);
+	}
+
+	private void OnDisconnect(object sender, string reason)
+    {
+		Disconnect();
+    }
+
+	public static void Disconnect()
+    {
+		if (instance.server != null)
+		{
+			instance.server.Disconnect();
+			instance.server = null;
+        }
+
+		instance.ShowMenu(Menu.Connect);
+    }
+
+    private void OnDestroy()
+    {
+		if (server != null)
+			Disconnect();
+    }
+
+	#region UI
+
+	[SerializeField]
+	private TMP_InputField username;
+
+	[SerializeField]
+	private TMP_InputField roomname;
+
+	[SerializeField]
+	private TextMeshProUGUI playerCount;
+
+	[SerializeField]
+	private GameObject connect;
+
+	[SerializeField]
+	private GameObject ready;
+
+	private enum Menu
+    {
+		None,
+		Connect,
+		Ready,
+		End
+    }
+
+	private void ShowMenu(Menu menu)
+    {
+		connect.SetActive(menu == Menu.Connect);
+		ready.SetActive(menu == Menu.Ready);
+    }
+
+	#endregion
+}
--- Serverside Code/Game Code/Game.cs
+++ Serverside Code/Game Code/Game.cs
@@ -4,94 +4,278 @@ using System;
 
 public class Ship
 {
-	int x;
-	int y;
-	int dir;
+    public readonly int x;
+    public readonly int y;
+    public readonly int dir;
 
-	bool[] state;
+    private bool[] state;
+    public int length => state.Length;
 
-	public Ship(int x, int y, int dir, int length)
+    public bool destroyed => Array.TrueForAll(state, s => s);
+
+    public Ship(int x, int y, int dir, int length)
     {
-		this.x = x;
-		this.y = y;
-		this.dir = dir;
+        this.x = x;
+        this.y = y;
+        this.dir = dir;
 
-		state = new bool[length];
-		for (int i = 0; i < length; ++i)
-			state[i] = true;
-	}
+        state = new bool[length];
+    }
 
-	public bool Intersect(Ship other)
-	{
-		int lx = Math.Abs(other.x - x);
-		int ly = Math.Abs(other.y - y);
-
-		return (((IsHorizontallyDirectedTo(other.x) && state.Length <= lx) ||
-			(other.IsHorizontallyDirectedTo(x) && other.state.Length <= lx)) &&
-			((IsVerticallyDirectedTo(other.y) && state.Length <= ly) ||
-			(other.IsVerticallyDirectedTo(y) && other.state.Length <= ly)));
-	}
-
-	public bool IsHorizontallyDirectedTo(int x)
+    public bool Shoot(int x, int y)
     {
-		return this.x == x ||
-			(dir == 0 && this.x < x) ||
-			(dir == 2 && this.x > x);
-	}
+        if (Intersect(x, y))
+        {
+            int index = Math.Abs(x - this.x) + Math.Abs(y - this.y);
 
-	public bool IsVerticallyDirectedTo(int y)
-	{
-		return this.y == y ||
-			(dir == 1 && this.y < y) ||
-			(dir == 3 && this.y > y);
-	}
+            state[index] = true;
+
+            return destroyed;
+        }
+
+        return false;
+    }
+
+    public bool Intersect(int x, int y)
+    {
+        return (this.y == y && IsHorizontallyDirectedTo(x) && state.Length >= Math.Abs(x - this.x)) ||
+            (this.x == x && IsVerticallyDirectedTo(y) && state.Length >= Math.Abs(y - this.y));
+    }
+
+    public bool Intersect(Ship other)
+    {
+        int x = other.x, y = other.y;
+        for (int i = 0; i < other.length; ++i)
+        {
+            if (Intersect(x, y))
+                return true;
+
+            if (other.dir == 0 || other.dir == 2)
+                x += other.dir == 0 ? 1 : -1;
+            else
+                y += other.dir == 1 ? 1 : -1;
+        }
+
+        return false;
+    }
+
+    public bool IsHorizontallyDirectedTo(int x)
+    {
+        return this.x == x ||
+            (dir == 0 && this.x < x) ||
+            (dir == 2 && this.x > x);
+    }
+
+    public bool IsVerticallyDirectedTo(int y)
+    {
+        return this.y == y ||
+            (dir == 1 && this.y < y) ||
+            (dir == 3 && this.y > y);
+    }
 }
 
 public class Player : BasePlayer
 {
-	List<Ship> ships = new List<Ship>();
+	public bool IsReady = false;
+    public int Index;
+
+	private List<Ship> ships = new List<Ship>();
+
+    public bool IsDead()
+    {
+        return ships.TrueForAll(s => s.destroyed);
+    }
+
+    public bool Shoot(int x, int y, out bool destroyed)
+    {
+        Ship ship = ships.Find(s => s.Intersect(x, y));
+
+        if (ship != null)
+        {
+            destroyed = ship.Shoot(x, y);
+
+            return true;
+        }
+
+        return destroyed = false;
+    }
 
 	public bool AddShip(int x, int y, int dir, int length)
 	{
-		return false;
+        Ship ship = new Ship(x, y, dir, length);
+
+        bool intersect = ships.Find(s => s.Intersect(ship)) != null;
+
+        if (intersect)
+            return false;
+
+        ships.Add(ship);
+
+        return true;
+    }
+
+    public bool RemoveShip(int x, int y)
+    {
+        Ship ship = ships.Find(s => s.Intersect(x, y));
+
+        if (ships != null)
+            return ships.Remove(ship);
+
+        return false;
     }
 }
 
 [RoomType("BattleChip")]
 public class GameCode : Game<Player>
 {
+	private List<Player> players;
+
+	private bool isRunning = false;
+
+	int current;
+
 	public override void GameStarted()
 	{
-		//RoomId
-		//AddTimer
-		//Players
-		//PlayerCount
-		//Broadcast
-
-		Console.WriteLine(new Ship(0, 0, 0, 4).Intersect(new Ship(2, 2, 3, 3)));
+		players = new List<Player>();
 	}
 
-	public override void GameClosed()
+    public override bool AllowUserJoin(Player player)
+    {
+		return !isRunning && players.Count <= 5;
+    }
+
+    public override void GameClosed()
 	{
 
 	}
 
 	public override void UserJoined(Player player)
 	{
-
+		players.Add(player);
 	}
 
 	public override void UserLeft(Player player)
 	{
-
+		if (isRunning)
+			players.ForEach(p => p.Disconnect());
 	}
 
-	public override void GotMessage(Player player, Message message)
+	public override void GotMessage(Player sender, Message message)
 	{
 		switch (message.Type)
 		{
-			case "Move":
+			case "Ready":
+			{
+				sender.IsReady = message.GetBoolean(0);
+
+				int count = players.FindAll(player => player.IsReady).Count;
+
+				Broadcast("Count", count, players.Count);
+
+				if (players.Count > 1 && count == players.Count)
+                {
+					isRunning = true;
+
+                    for (int i = 0; i < players.Count; ++i)
+                    {
+                        players[i].Index = i;
+                        players[i].Send("Board", i);
+                        players[i].IsReady = false;
+                    }
+                }
+
 				break;
-		}
+			}
+
+            case "Add":
+            {
+                int x = message.GetInt(0);
+                int y = message.GetInt(1);
+                int dir = message.GetInt(2);
+                int length = message.GetInt(3);
+
+                sender.AddShip(x, y, dir, length);
+
+                break;
+            }
+
+            case "Remove":
+            {
+                int x = message.GetInt(0);
+                int y = message.GetInt(1);
+
+                sender.RemoveShip(x, y);
+
+                break;
+            }
+
+            case "Boarded":
+            {
+                sender.IsReady = true;
+
+                int count = players.FindAll(player => player.IsReady).Count;
+
+                Broadcast("Count", count, players.Count);
+
+                if (players.Count > 1 && count == players.Count)
+                {
+                    Broadcast("Play");
+
+                    current = 0;
+
+                    foreach (Player player in players)
+                        player.IsReady = false;
+                }
+
+                break;
+            }
+
+            case "Shoot":
+            {
+                if (current == sender.Index)
+                {
+                    int id = message.GetInt(0);
+
+                    if (id >= 0 && id != sender.Index && id < players.Count)
+                    {
+                        int x = message.GetInt(1);
+                        int y = message.GetInt(2);
+
+                        bool touched = players[id].Shoot(x, y, out bool destroyed);
+
+                        Broadcast("Shoot", id, x, y, touched, destroyed);
+
+                        if (players[id].IsDead())
+                            Broadcast("Dead", id);
+
+                        ++current;
+
+                        int count = 0, winner = -1;
+                        foreach (Player player in players)
+                        {
+                            if (!player.IsDead())
+                            {
+                                ++count;
+                                winner = player.Index;
+                            }
+                        }
+
+                        if (count == 1)
+                            Broadcast("End", winner);
+                    }
+                }
+
+                break;
+            }
+
+            case "Count":
+            {
+                int count = players.FindAll(player => player.IsReady).Count;
+
+                sender.Send("Count", count, players.Count);
+
+                break;
+            }
+        }
 	}
 }
